40850fe498d29421c3f644d8843a9b8f6687dc09
YXLANDBXH <1722756751@qq.com>
2021-07-09 09:49:22 +0800

    第一次提交代码

---
+++ Test/src/com/xl/test/DemoTest1.java
@@ -0,0 +1,16 @@
+package com.xl.test;
+
+/**
+ * @author XLong
+ * @create 2021-07-09 9:47
+ */
+public class DemoTest1 {
+    public static void main(String[] args) {
+        System.out.println("Hello！！！！！");
+        System.out.println("Hello！！！！！");
+        System.out.println("Hello！！！！！");
+        System.out.println("Hello！！！！！");
+        System.out.println("Hello！！！！！");
+        System.out.println("Hello！！！！！");
+    }
+}
